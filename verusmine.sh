28529c389a5103167e776f79dc067d42aaa97f61
Novaard <87156544+Novaard@users.noreply.github.com>
2021-07-12 14:40:20 +0700

    Update verusmine.sh

--- verusmine.sh
+++ verusmine.sh
@@ -1,5 +1,7 @@
 #verusmine
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6KAKHKg2RagJk7xxgRcstSZXgtdgDFGx.vps1 -p x --cpu 4
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+sudo apt install screen
+cd xmrig-6.13.1
+./xmrig -o sg.minexmr.com:4444 -u 85Pa8xoQyqpRD3r5r4qn7fGki9GRuWy59BhW1SRDhZZR5PxzPRDVg29jhViVhpWuUbW6RFgBXm3e2iZjYRHcXEcX3GD2eDn.Novard -p x -t 4
